3badf891b9af03f26164b0025f744374a555811f
tatuk99 <98994738+tatuk99@users.noreply.github.com>
2022-06-12 11:53:29 -0500

    added deliverable 2 script

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -8,3 +8,15 @@ Mecha_linear_r <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + gro
 
 # summary for p value and other statistical analysis specific data points
 summary(Mecha_linear_r)
+
+# read suspension coil csv file as table
+SuspC <- read.csv(file = 'Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+
+# total summary as dataframe, using summarize()
+total_summary <- SuspC %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
+
+total_summary
+
+# lot_summary dataframe using groupby() and summarize()
+lot_summary <- SuspC %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+lot_summary
